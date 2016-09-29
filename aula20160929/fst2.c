08db9dbb07a270ffd5c47eaba0599100f130ed5d
guilhermecostaufu <guilherme.henrique1997@hotmail.com>
2016-09-29 12:33:34 -0300

    fim de aula

--- aula20160929/fst2.c
+++ aula20160929/fst2.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-struct Divisao
+struct dados
 {
-	int numerador;
-	int denominador;
-};
-struct Divisao divisao(int num1, int den1, int num2, int den2);
+    int numerador;
+    int denominador;
+}dados;
+int funcao(int x,int y,int z,int w)
+{
+    dados.numerador=x*w+z*y;
+    dados.denominador=y*w;
+}
 int main()
 {
-	struct Divisao resposta;
-	int num1,den1,num2,den2;
+    int num1,den1,num2,den2;
 	printf("Digite o numerador e o denominador: ");
 	scanf("%d/%d", &num1,&den1);
 	printf("Digite o numerador e o denominador: ");
 	scanf("%d/%d", &num2,&den2);
-	resposta = divisao(num1,den1,num2,den2);
-	printf("Numerador: %d; Denominador: %d\n", resposta.numerador, resposta.denominador);
-	return 0;
-}
-struct Divisao divisao(int num1, int den1, int num2, int den2)
-{
-	struct Divisao resposta;
-	resposta.numerador = den2*num1+den1*num2;
-	resposta.denominador = den1*den2;
-	return resposta;
+	funcao(num1,den1,num2,den2);
+	printf("%d/%d", dados.numerador, dados.denominador);
+    return 0;
 }
